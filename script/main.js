dcaf36180a918c4e63079aefa30e5dcc2e472827
mathesfonseca18 <matheus.fonseca0218@gmail.com>
2023-08-09 01:32:40 -0300

    arrumando menu responsivo

--- script/main.js
+++ script/main.js
@@ -1,5 +1,27 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    let menu = document.querySelector('.menu');
-    menu.style.display = menu.style.display === 'none' ? 'flex' : 'none';
-  });
+// declarando variavel pra ser usada
+const menuMobile = document.querySelector(".mobile-menu");
+
+function menuShow() {
+  // se (verdadeiro) elemento da atribuida a varoavel tem a classe, remova a classe e muda a imagem 
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".menu-icon").src = "img/icones/menu.png";
+    // se (falso) nao tem, adiciona a classe e mude a imagem
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".menu-icon").src = "img/icones/close.png";
+  }
+}
+
+window.onclick = (event) => {
+  if (!event.target.matches(".menu-icon")) {
+    if (menuMobile.classList.contains("open")) {
+      menuMobile.classList.remove("open");
+      document.querySelector(".menu-icon").src = "img/icones/menu.png";
+    }
+  }
+};
+
+menuMobile.addEventListener("click", (event) => event.stopPropagation());
+
   
